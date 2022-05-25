95c34963fd954bc5da339a15929ac31fd5ffb0b0
4kord <57727313+4kord@users.noreply.github.com>
2022-05-25 23:44:52 +0300

    .

---
+++ api/internal/controllers/usercontroller.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/4kord/go-react-auth/internal/dto"
+	"github.com/4kord/go-react-auth/internal/logger"
+	"github.com/4kord/go-react-auth/internal/core/services/users"
+)
+
+type UserController struct{
+    Service users.Service
+}
+
+func (c UserController) Register(w http.ResponseWriter, r *http.Request) {
+    var request dto.UserRequest
+
+    err := json.NewDecoder(r.Body).Decode(&request)
+    if err != nil {
+        logger.ErrorLog.Println(err.Error())
+		writeResponse(w, http.StatusBadRequest, err.Error())
+        return
+    }
+
+    e := c.Service.Register(request)
+    if e != nil {
+        logger.ErrorLog.Println(e.Message)
+		writeResponse(w, e.Code, e)
+        return
+    }
+
+    writeResponse(w, http.StatusCreated, nil)
+}
