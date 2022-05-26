67e73f7087230951a08d14d7d20d884211ecdfd7
4kord <57727313+4kord@users.noreply.github.com>
2022-05-26 15:51:45 +0300

    db schema

--- api/dbschemes/01.sql
+++ api/dbschemes/01.sql
@@ -4,4 +4,5 @@ CREATE TABLE users (
 	password VARCHAR(255) NOT NULL,
 	role VARCHAR(255) DEFAULT('user') NOT NULL
 );
-ALTER TABLE users ADD CONSTRAINT uc_users_username UNIQUE(username);
+ALTER TABLE users
+ADD CONSTRAINT uc_users_username UNIQUE(username);
