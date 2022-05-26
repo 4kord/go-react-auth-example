206f72e09db6f80d9c268bb3c899adbc92259803
4kord <57727313+4kord@users.noreply.github.com>
2022-05-26 09:47:44 +0300

    code refactoring

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"os"
 
-	"github.com/4kord/go-react-auth/internal/http/app"
 	"github.com/4kord/go-react-auth/internal/http/db"
+	"github.com/4kord/go-react-auth/internal/http/routes"
 	"github.com/4kord/go-react-auth/internal/logger"
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
+
 
 func main() {
 	err := godotenv.Load("/Users/4kord/Documents/Projects/go-react-auth/api/config/.env")
@@ -21,8 +23,26 @@ func main() {
 		logger.ErrorLog.Fatal(err)
     }
 
-    app.Run(app.Config{
-        Port: os.Getenv("PORT"),
-        Db: db,
+    app := fiber.New(fiber.Config{
+        ErrorHandler: func(c *fiber.Ctx, err error) error {
+            code := fiber.StatusInternalServerError
+            if e, ok := err.(*fiber.Error); ok {
+                code = e.Code
+            }
+
+            c.Status(code)
+            err = c.JSON(fiber.Map{
+                "status": "error",
+                "code": code,
+                "message": err.Error(),
+            })
+            if err != nil {
+                return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
+            }
+
+            return nil
+        },
     })
+
+    logger.ErrorLog.Fatal(routes.Setup(app, db).Listen(os.Getenv("PORT")))
 }
---
+++ api/internal/http/routes/routes.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"database/sql"
+
+	usersrepo "github.com/4kord/go-react-auth/internal/core/repositories/users"
+	usersservice "github.com/4kord/go-react-auth/internal/core/services/users"
+	"github.com/4kord/go-react-auth/internal/http/controllers"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+func Setup(app *fiber.App, db *sql.DB) *fiber.App {
+	userController := controllers.UserController{
+		Service: usersservice.New(usersrepo.New(db)),
+	}
+
+    app.Use(recover.New())
+    app.Use(logger.New())
+    app.Use(cors.New())
+    app.Use(csrf.New())
+
+    app.Post("/api/auth/register", userController.Register)
+
+    return app
+}
